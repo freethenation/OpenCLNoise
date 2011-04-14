9107537d6d394b042aaee648dab478549b4ca957
Eric Swanson <eswanson@alloscomp.com>
2011-04-14 14:32:52 -0400

    fixed previoius bug - work on lab 2

--- Lab2/lab2.py
+++ Lab2/lab2.py
@@ -73,7 +73,8 @@ if len(devices) == 1:
 if not device:
     device = askLongOptions("Select a device",devices)
     
-print device.get_info(cl.device_info.LOCAL_MEM_SIZE)
+# Find size of local memory:
+# device.get_info(cl.device_info.LOCAL_MEM_SIZE)
     
 # LAB-SPECIFIC CODE AFTER THIS
 
